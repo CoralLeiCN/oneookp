65b22a46adf1f639868ed5a5058b1929979e4b88
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-11-05 22:32:21 +0000

    Add num-traits dependency and make zeros_with_dim generic for element types

--- ndarray_quickstart/src/main.rs
+++ ndarray_quickstart/src/main.rs
@@ -1,23 +1,30 @@
 use ndarray::Array;
 use ndarray::Dimension;
-use ndarray::Shape;
+// use ndarray::Shape;
 use ndarray::ShapeBuilder;
+use num_traits::Zero;
+
+// trait Real {}
+
+// impl Real for f32 {}
+// impl Real for f64 {}
 
 //the zero with dim is generic over some type Sh
-fn zeros_with_dim<Sh>(dim: Sh) -> Array<f64, <Sh as ShapeBuilder>::Dim>
+fn zeros_with_dim<T, Sh>(dim: Sh) -> Array<T, <Sh as ShapeBuilder>::Dim>
 where
+    T: Zero + Clone,
     Sh: ShapeBuilder<Dim: Dimension>,
 {
-    // # original codes examples
-    // let a = Array::<f64, _>::zeros((1, 3, 2).f());
-
-    // # dimensionality explicitly Array::<f64, Ix3>::zeros(...), withIx3
-    // let a = Array::<f64, Ix3>::zeros((1, 3, 2).f());
-
-    // # f()) makes a column major, so without f() it will be row major
-    // let a = Array::<f64, Ix3>::zeros((1, 3, 2));
-    Array::<f64, _>::zeros(dim)
+    Array::<T, _>::zeros(dim)
 }
+// # original codes examples
+// let a = Array::<f64, _>::zeros((1, 3, 2).f());
+
+// # dimensionality explicitly Array::<f64, Ix3>::zeros(...), withIx3
+// let a = Array::<f64, Ix3>::zeros((1, 3, 2).f());
+
+// # f() makes a column major, so without f() it will be row major
+// let a = Array::<f64, Ix3>::zeros((1, 3, 2));
 
 fn print_type<T>(_: &T) {
     println!("{:?}", std::any::type_name::<T>());
@@ -25,9 +32,11 @@ fn print_type<T>(_: &T) {
 fn main() {
     // The Array<A, D> is parameterized by A for the element type and D for the dimensionality.
     // through turbofish syntax, and let it infer the dimensionality type
-    let a = zeros_with_dim((1, 3, 2));
+    let a: Array<f32, _> = zeros_with_dim((1, 3, 2));
     println!("{:?}", a);
 
+    let a: Array<f64, _> = zeros_with_dim((1, 3, 2));
+    println!("{:?}", a);
     let b = (1, 3, 2);
     print_type(&b);
 }
