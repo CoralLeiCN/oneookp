7482e03fc115ba9e825a8fe717c6bb1c774dd973
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-11-08 23:38:17 +0000

    move the example of array::zeros

--- ndarray_quickstart/src/main.rs
+++ ndarray_quickstart/src/main.rs
@@ -16,16 +16,17 @@ where
     T: Zero + Clone,
     Sh: ShapeBuilder<Dim: Dimension>,
 {
+    // # original codes examples
+    // let a = Array::<f64, _>::zeros((1, 3, 2).f());
+
+    // # dimensionality explicitly Array::<f64, Ix3>::zeros(...), withIx3
+    // let a = Array::<f64, Ix3>::zeros((1, 3, 2).f());
+
+    // # f() makes a column major, so without f() it will be row major
+    // let a = Array::<f64, Ix3>::zeros((1, 3, 2));
+
     Array::<T, _>::zeros(dim)
 }
-// # original codes examples
-// let a = Array::<f64, _>::zeros((1, 3, 2).f());
-
-// # dimensionality explicitly Array::<f64, Ix3>::zeros(...), withIx3
-// let a = Array::<f64, Ix3>::zeros((1, 3, 2).f());
-
-// # f() makes a column major, so without f() it will be row major
-// let a = Array::<f64, Ix3>::zeros((1, 3, 2));
 
 fn print_type<T>(_: &T) {
     println!("{:?}", std::any::type_name::<T>());
