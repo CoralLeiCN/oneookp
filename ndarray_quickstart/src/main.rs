5a048f5c40e59a0cf5c2796f4698cbd6fb68c896
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-11-27 23:47:31 +0000

    Add array splitting example and demonstrate shared view in ndarray

--- ndarray_quickstart/src/main.rs
+++ ndarray_quickstart/src/main.rs
@@ -182,4 +182,20 @@ fn main() {
 
     let c = b.into_shape_with_order([6, 2]).unwrap(); // consume b and generate c with new shape
     println!("c = \n{:?}", c);
+
+    let mut a = Array::range(0., 12., 1.)
+        .into_shape_with_order([3, 4])
+        .unwrap();
+    println!("a = \n{}\n", a);
+
+    {
+        let (s1, s2) = a.view().split_at(Axis(1), 2);
+
+        // with s as a view sharing the ref of a, we cannot update a here
+        // a.slice_mut(s![1, 1]).fill(1234.);
+
+        println!("Split a from Axis(0), at index 1:");
+        println!("s1  = \n{}", s1);
+        println!("s2  = \n{}\n", s2);
+    }
 }
