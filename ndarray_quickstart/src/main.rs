2f29a3c6a67106bc32360f2538e69c06d8de4e88
CoralLeiCN <19520698+CoralLeiCN@users.noreply.github.com>
2024-11-28 23:19:29 +0000

    broadcasting

--- ndarray_quickstart/src/main.rs
+++ ndarray_quickstart/src/main.rs
@@ -198,4 +198,15 @@ fn main() {
         println!("s1  = \n{}", s1);
         println!("s2  = \n{}\n", s2);
     }
+    {
+        let a = array![[1., 1.], [1., 2.], [0., 3.], [0., 4.]];
+
+        let b = array![[0., 1.]];
+
+        let c = array![[1., 2.], [1., 3.], [0., 4.], [0., 5.]];
+
+        // We can add because the shapes are compatible even if not equal.
+        // The `b` array is shape 1 × 2 but acts like a 4 × 2 array.
+        assert!(c == a + b);
+    }
 }
